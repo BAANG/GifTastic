e1267d2e8eb5195a2865c3ba1ca728300a9044d8
BAANG <chcelestino@gmail.com>
2019-06-05 23:40:08 -0700

    Add responsiveness to generated images

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,12 +24,12 @@ $(document).on("click", "button", function() { // On click event listener for <b
     var topic = $(this).attr("data-topic");
     console.log(topic)
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    topic + "&api_key=5FzCS8nECS5DZZ0WLDqTc8yZ7NZSLg82&limit=10";
+    topic + "&api_key=5FzCS8nECS5DZZ0WLDqTc8yZ7NZSLg82&limit=12";
     
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function(response) { // promise function to generate images to page after ajax get method
         
         console.log(response);
         
@@ -37,22 +37,22 @@ $(document).on("click", "button", function() { // On click event listener for <b
         
         for (var i = 0; i < results.length; i++) {
             
-            var gifDiv = $('<div>')
-            
             var p =  $('<p>')
-            $(p).text("Rating: " + results[i].rating)
+                $(p).text("Rating: " + results[i].rating)
             
             var gifImage = $('<img class="giphy">')
-            gifImage.attr("data-state", "still")
-            gifImage.attr("src", results[i].images.fixed_height_still.url)
-            gifImage.attr("data-animate", results[i].images.fixed_height.url)
-            gifImage.attr("data-still", results[i].images.fixed_height_still.url)
+                gifImage.attr("data-state", "still")
+                gifImage.attr("src", results[i].images.fixed_height_still.url)
+                gifImage.attr("data-animate", results[i].images.fixed_height.url)
+                gifImage.attr("data-still", results[i].images.fixed_height_still.url)     
             
-            $(gifDiv).addClass("col-6")
-            $(gifDiv).append(p)
-            $(gifDiv).append(gifImage)
+            var gifDiv = $('<div>')
+                $(gifDiv).addClass("col-lg-4 col-md-6 col-sm-12")
+                $(gifDiv).append(p)
+                $(gifDiv).append(gifImage)
+                
+                $("#gifPlace").prepend(gifDiv)
             
-            $("#gifPlace").prepend(gifDiv)
         }
     });
 });
@@ -71,5 +71,5 @@ $(document).on("click", ".giphy", function() { //Event listner for GIFs --CLICK 
 
     
 $(document).ready(function() { // On document load...
-    // TODO: Create functional dynamic button generator
+    
 });
