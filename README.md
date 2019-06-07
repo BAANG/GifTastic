1c2505e0ef4b876a631907f69d311733da27be0f
BAANG <chcelestino@gmail.com>
2019-06-06 21:21:01 -0700

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # GifTastic
+
+This is an assignment for UC Berkeley Coding Bootcamp to create and deploy a webpage that makes Ajax calls to the Giphy API to dynamically generate GIfs according the the user's own search queries.
+
+To create this webpage, I had to utilize jQuery and the jQuery Ajax method in order to make dynamically generated elements (both the .gif images and the buttons), and create event listeners that listened to the dynamically generated elements in order to both generate new .gif images and animate them.
+
+<img src="https://via.placeholder.com/300x200" alt="Image of Deployed Website"/>
+
+---
+### Technologies Used
+* Git
+* HTML
+* CSS
+    * [@Bootstrap](https://getbootstrap.com) CSS Libraray
+* Javascript
+    * jQuery JS Library
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,39 +6,30 @@ $("#add-gif").on("click", function(event) { // On click event listener for add g
     // prevent form from submitting
     event.preventDefault();
 
-
     var searchTerm = $("#search-gif").val().trim();
     console.log(searchTerm);
 
-    if (!searchTerm) { // Does not allow for empty search value
-        return
-    } else {
-        var newButton = $("<button>")
-        newButton.addClass("btn btn-primary gifBtn");
-        newButton.attr("data-topic", searchTerm);
-        newButton.text(searchTerm);
-    
-        $("#button-place").after(newButton);
-    
-        $("#search-gif").val("");
-    }
+    var newButton = $("<button>")
+    newButton.addClass("btn btn-primary");
+    newButton.attr("data-topic", searchTerm);
+    newButton.text(searchTerm);
+
+    $("#button-place").after(newButton);
+
+    $("#search-gif").val("");
 });
 
-$("#clear-gif").on("click", function() {
-    $("#gifPlace").empty();
-})
 
-
-$(document).on("click", ".gifBtn", function() { // On click event listener for <button> tags
+$(document).on("click", "button", function() { // On click event listener for <button> tags
     var topic = $(this).attr("data-topic");
     console.log(topic)
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    topic + "&api_key=5FzCS8nECS5DZZ0WLDqTc8yZ7NZSLg82&limit=12";
+    topic + "&api_key=5FzCS8nECS5DZZ0WLDqTc8yZ7NZSLg82&limit=10";
     
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) { // promise function to generate images to page after ajax get method
+    }).then(function(response) {
         
         console.log(response);
         
@@ -46,22 +37,23 @@ $(document).on("click", ".gifBtn", function() { // On click event listener for <
         
         for (var i = 0; i < results.length; i++) {
             
+            var gifDiv = $('<div>')
+            
             var p =  $('<p>')
-                $(p).text("Rating: " +  results[i].rating)
+            $(p).text("Rating: " + results[i].rating)
             
             var gifImage = $('<img class="giphy">')
-                gifImage.attr("data-state", "still")
-                gifImage.attr("src", results[i].images.fixed_height_still.url)
-                gifImage.attr("data-animate", results[i].images.fixed_height.url)
-                gifImage.attr("data-still", results[i].images.fixed_height_still.url)     
+            gifImage.attr("data-state", "still")
+            gifImage.attr("src", results[i].images.fixed_height_still.url)
+            gifImage.attr("data-animate", results[i].images.fixed_height.url)
+            gifImage.attr("data-still", results[i].images.fixed_height_still.url)
             
-            var gifDiv = $('<div>')
-                $(gifDiv).addClass("col-lg-4 col-md-6 col-xs-12")
-                $(gifDiv).append(p)
-                $(gifDiv).append(gifImage)
-                
-                $("#gifPlace").prepend(gifDiv)
+            $(gifDiv).addClass("col-6")
+            $(gifDiv).append(p)
+            $(gifDiv).append(gifImage)
             
+            $("#gifPlace").prepend(gifDiv)
+
         }
     });
 });
@@ -80,5 +72,5 @@ $(document).on("click", ".giphy", function() { //Event listner for GIFs --CLICK 
 
     
 $(document).ready(function() { // On document load...
-    
+    // TODO: Create functional dynamic button generator
 });
